97a5f71d7aa41dff6988246ef24fc871de10c267
周春花 <zhouchunhua@zbj.com>
2018-04-11 17:22:39 +0800

    Initial commit

---
+++ src/main/java/com/flower/fancyutils/Test.java
@@ -0,0 +1,13 @@
+package com.flower.test;
+
+/**
+ * @author nicolee 2018/3/29
+ */
+
+public class Test {
+    public static void main(String[] args) {
+      System.out.println("---------upload to github --------");
+
+    }
+
+}
